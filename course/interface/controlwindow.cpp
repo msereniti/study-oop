530bd74a88aa8aa1ec799117813d1bac270d93f6
Mikhail Kryuchkov <phytonmk@yandex-team.ru>
2021-04-08 22:44:10 +0300

    fixed control window buttons height

--- course/interface/controlwindow.cpp
+++ course/interface/controlwindow.cpp
@@ -8,13 +8,13 @@ TControlWindow::TControlWindow(bool m, QWidget *parent) : QWidget(parent) {
   setEnabled(m);
 
   b1 = new QPushButton("Сброс", this);
-  b1->setGeometry(10, 10, 180, 20);
+  b1->setGeometry(10, 10, 180, 30);
   connect(b1, SIGNAL(pressed()), this, SIGNAL(reset()));
   b2 = new QPushButton("Одиночное событие", this);
-  b2->setGeometry(10, 40, 180, 20);
+  b2->setGeometry(10, 40, 180, 30);
   connect(b2, SIGNAL(pressed()), this, SIGNAL(step()));
   b3 = new QPushButton("Автоматический режим", this);
-  b3->setGeometry(10, 70, 180, 20);
+  b3->setGeometry(10, 70, 180, 30);
   connect(b3, SIGNAL(pressed()), this, SLOT(switchMode()));
 }
 
